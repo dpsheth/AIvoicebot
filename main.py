ac9a231930fadb6cb8629f7e7535442f009131e8
dshethcodes <129557680+dpsheth@users.noreply.github.com>
2024-02-29 10:31:54 -0800

    made the remainder functions for voicebot to work and made response based on 0.28 openai version

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-from gtts import gtts
+from gtts import gTTS
 from playsound import playsound
 from pygame import mixer
 from io import BytesIO
@@ -8,7 +8,7 @@ import openai
 
 openai.api_key=''
 messages_array = [
-    {'role': 'system', 'content': 'You are my friend, Thomas'}
+    {'role': 'system', 'content': 'You are my friend, Jessica'}
 ]
 
 def listen():
@@ -19,8 +19,32 @@ def listen():
         audio = r.listen(source)
     try:
         print('Recognizing...')
-        query = r.recognize_google(audio, 'en-in')
+        query = r.recognize_google(audio, language='en-in')
         print(f'user has said {query}')
         messages_array.append({'role': 'user', 'content': query})
         respond(audio)
-    except:
+    except Exception as e:
+        print('Say that again please...', e)
+        
+def respond(audio):
+    print('Responding...')
+    res=openai.Completion.create(
+        model = 'gpt-3.5-turbo',
+        messages=messages_array
+    )
+
+    res_message = res.choices[0].message
+    messages_array.append(res_message)
+
+    speak(res_message.content)
+
+def speak(text) :
+    speech = gTTS(text=text, lang='en', slow=False)
+    speech.save('captured_voice.mp3')
+    playsound('captured_voice.mp3')
+
+    os.remove('captured_voice.mp3')
+    listen()
+
+
+query = listen()
